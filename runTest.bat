a65c4e712f444e604f2ff8fcd514c58ded0a2e44
curse-github <curse@simpsoncentral.com>
2025-10-01 23:39:51 -0500

    fixed on linux again

--- runTest.bat
+++ runTest.bat
@@ -3,10 +3,10 @@ del /Q .\out\*
 del /Q .\out
 mkdir out
 echo target triple = "x86_64-pc-windows-msvc19.37.32825" >> .\out\test.ll
-type .\test.ll >> .\out\test.ll
-clang++ -O3 -emit-llvm -S ./cppPrintLib.cpp -o ./out/cppPrintLib.ll
+type .\src\test.ll >> .\out\test.ll
+clang++ -O3 -emit-llvm -S ./lib/cppPrintLib.cpp -o ./out/cppPrintLib.ll
 clang++ ./out/cppPrintLib.ll -c -o ./out/cppPrintLib.obj
-clang++ ./printLibWin.ll -c -o ./out/printLib.obj
+clang++ ./lib/printLibWin.ll -c -o ./out/printLib.obj
 clang++ ./out/test.ll -c -o ./out/test.obj
 clang++ ./out/cppPrintLib.obj ./out/printLib.obj ./out/test.obj -o ./out/test.exe
 out\test.exe
--- runTest.sh
+++ runTest.sh
@@ -1,8 +1,11 @@
+rm -R out
 mkdir out
-clang++ -O3 -fdeclspec -emit-llvm -S ./cppPrintLib.cpp -o ./out/cppPrintLib.ll
+echo target triple = \"x86_64-pc-linux-gnu\" >> ./out/test.ll
+cat ./src/test.ll >> ./out/test.ll
+clang++ -O3 -fdeclspec -emit-llvm -S ./lib/cppPrintLib.cpp -o ./out/cppPrintLib.ll
 clang++ ./out/cppPrintLib.ll -c -o ./out/cppPrintLib.o
-clang++ ./printLibLin.ll -c -o ./out/printLib.o
-clang++ ./test.ll -c -o ./out/test.o
+clang++ ./lib/printLibLin.ll -c -o ./out/printLib.o
+clang++ ./out/test.ll -c -o ./out/test.o
 clang++ ./out/cppPrintLib.o ./out/printLib.o ./out/test.o -o ./out/test.out
 out/test.out
 echo test exited with code $?
